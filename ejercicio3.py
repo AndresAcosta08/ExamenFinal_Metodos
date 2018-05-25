6ef1b58f04953cc1b1865178b8081744af097a9d
AndresAcosta08 <37352270+AndresAcosta08@users.noreply.github.com>
2018-05-24 20:06:48 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,3 +7,10 @@ import numpy as np
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+plt.plot(t,u,color ="y")
+plt.plot(t,u,color ="r")
+plt.title("Serie en funcion de tiempo")
+plt.xlabel("Tiempo")
+plt.ylabel("Serie")
+plt.show
