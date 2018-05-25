eefdcae3cf1beccd938f06fd3ffcd6ead2b20b2b
AndresAcosta08 <37352270+AndresAcosta08@users.noreply.github.com>
2018-05-24 20:05:38 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,8 +3,16 @@
 # los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
 import numpy as np
 
-x = np.int_(np.random.random(100)*1000)
-print(x)
+#Creo un rango muy grande, para correr multiples veces el codigo.
+for i in range (100000)
+#Para cada iteracion del codigo calculo un x, que sera un numero aleatorio.
+  x= np.int_(np.random.random(100)*1000)
+#Si el numero es impar entonces lo imprimo
+  if (x%2!=0):
+    print x
+#Si el numero es superior a 800 creo un break para romper el ciclo del for.
+  if (x>800):
+    break
 
 
 
